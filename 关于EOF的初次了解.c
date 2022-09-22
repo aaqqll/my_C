e998344cbda4df135ead31ebb43683ca33596a26
AQL <113401981+aaqqll@users.noreply.github.com>
2022-09-22 21:09:44 +0800

    Add files via upload

---
+++ 关于EOF的初次了解.c
@@ -0,0 +1,24 @@
+//#include <stdio.h>
+//int main()
+//{
+//    char t, arr[10] = "AaEeIiUuOo";
+//    while (scanf_s("%c", &t) != EOF)
+//    {
+//        if (t != '\n')
+//        {
+//            int j = 0;
+//            int m = 0;
+//            for (j = 0; j < 10; j++)
+//            {
+//                if (t == arr[j])
+//                    m++;
+//            }
+//            if (m != 0)
+//                printf("Vowel\n");
+//            else
+//                printf("Consonant\n");
+//        }
+//    }
+//
+//    return 0;
+//}
